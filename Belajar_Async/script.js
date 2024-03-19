f8a837a35ec7fd2f06e9f65ce1f7d1b5b0547c7e
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2024-03-19 09:45:15 +0700

    Async - setInterval

--- Belajar_Async/script.js
+++ Belajar_Async/script.js
@@ -6,6 +6,9 @@ function buttonClick() {
     // setTimeout(function() {
     //     callback();
     // }, 5000);
-    setTimeout(callback, 5000);
+    // setTimeout(callback, 5000);
+    setInterval(function() {
+        callback();
+    }, 2000);
     console.log('Success click button');
 }
