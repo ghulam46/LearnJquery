e2753b2290c7c2cfd2695e6ad4b0092d85cdc7bb
Ghulam Ammar Yanuar <65816301+ghulam46@users.noreply.github.com>
2024-03-19 10:25:05 +0700

    Async - initial commit for ajax

--- Belajar_Async/script.js
+++ Belajar_Async/script.js
@@ -1,3 +1,4 @@
+// CALLBACK
 function callback() {
     console.log('Hello world!');
 }
@@ -11,4 +12,43 @@ function buttonClick() {
         callback();
     }, 2000);
     console.log('Success click button');
+}
+
+
+// AJAX
+function getProductsUrl(keyword) {
+    return `https://www.blibli.com/backend/search/products?searchTerm=${keyword}`;
+}
+
+function getProducts(keyword) {
+    // Code AJAX Here!
+    const ajax = new XMLHttpRequest();
+    const url = getProductsUrl(keyword);
+    ajax.open("GET", url);
+    ajax.send();
+
+    // tidak bisa dilakukan secara sync
+    // const response = JSON.parse(ajax.responseText);
+    // console.log(response)
+}
+
+function clearProducts() {
+    const productUl = document.getElementById("products");
+    productUl.textContent = "";
+}
+
+function displayProducts(data) {
+    data.data.products.forEach(product => displayProduct(product));
+}
+
+function displayProduct(product) {
+    const productLi = document.createElement("li");
+    productLi.textContent = product.name;
+
+    const productUl = document.getElementById("products");
+    productUl.appendChild(productLis);
+}
+
+function buttonAjax() {
+    getProducts(document.getElementById("keyword").value);
 }
